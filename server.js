d49628c16dd1f99269f5fcde2a9f1d4a3bc070a8
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-11-11 23:03:11 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const aoijs = require('aoi.js');
+
+ const bot = new aoijs.Bot({
+   token: "yourtoken",
+   prefix: "yourprefix",
+   intents: "all",
+   SuppressPackageErrors: true
+ });
+
+bot.onMessage() //enables bot to see messages (required for executing Commands)
+bot.status({
+  text: "Made by 💻снайпер060⌨#1913",
+  type: "PLAYING",
+  status: "dnd",
+  time: 12
+})
+    
+bot.onInteractionCreate()
+
+bot.command({
+name: "create",
+code: `$createApplicationCommand[global;info;Information concernant le bot;true;slash;message:sends a message:true:3]
+$suppressErrors[]
+`
+})
+bot.command({
+name: "rthtjrjj",
+code: `
+$getApplicationCommandId[info]
+$deleteSlashCommand[global;$getApplicationCommandId[test]]`
+/*
+    Code Breakdown:
+This will delete our created slashcommand that we made.
+*/
+})
+
+bot.interactionCommand({
+name: "info", 
+prototype : 'slash',
+code:`
+$interactionReply[Badge]
+`
+})
